5794462f88fbac56197627cc8e27e90062981e94
Spencer Armini <spencerarmini@gmail.com>
2023-09-10 17:19:01 -0700

    reverting treatment test routes file back to basics on main
    created a branch to house this code instead for now

--- shotstuff/treatments/tests/test_routes.py
+++ shotstuff/treatments/tests/test_routes.py
@@ -5,18 +5,12 @@ sys.path.append('../../../')
 import unittest
 # import datetime
 from flask_login import FlaskLoginClient
-from flask import session
 
-from shotstuff import app, login_manager
-from shotstuff.database import db, connect_db
+from shotstuff import app
+from shotstuff.database import db
 from shotstuff.config import DATABASE_URL_TEST
 from shotstuff.treatments.factories import TreatmentFactory
-from shotstuff.medication_regimens.factories import MedicationRegimenFactory
-from shotstuff.injections.factories import InjectionFactory
 from shotstuff.fills.factories import FillFactory
-
-from shotstuff.treatments.models import Treatment
-from shotstuff.users.models import User
 
 app.test_client_class = FlaskLoginClient
 app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URL_TEST
@@ -24,9 +18,6 @@ app.config["TESTING"] = True
 app.config["SQLALCHEMY_ECHO"] = False
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['WTF_CSRF_ENABLED'] = False
-# login_manager.session_protection = None
-
-# connect_db(app)
 
 db.drop_all()
 db.create_all()
@@ -35,24 +26,9 @@ db.create_all()
 class TreatmentRouteTestCase(unittest.TestCase):
 
     def setUp(self):
-        # app.test_client_class = FlaskLoginClient
+
         self.t1 = TreatmentFactory()
         self.f1 = FillFactory()
-        # self.u1 = self.t1.user
-        # self.client = app.test_client(user=self.u1)
 
     def tearDown(self):
         db.session.rollback()
-
-    def test_treatment_listing_page(self):
-        """Tests that active treatments are listed appropriately."""
-
-        user = User.query.first()
-        breakpoint()
-        # app.test_client_class = FlaskLoginClient
-        with app.test_client(user=user, fresh_login=True) as client:
-
-            response = client.get(f'/treatments/users/{user.id}')
-            html = response.get_data(as_text=True)
-
-            self.assertEqual(response.status_code, 200)
