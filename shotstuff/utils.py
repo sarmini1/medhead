bd24e5cb57ed41ac74c607c0dcf57b0d63c98b87
Spencer Armini <spencerarmini@gmail.com>
2024-01-14 15:01:55 -0800

    corrected over-indentation

--- shotstuff/utils.py
+++ shotstuff/utils.py
@@ -25,29 +25,29 @@ def calculate_date(start_date=datetime.utcnow(), months_in_future=0):
     return future_date
 
 def generate_friendly_date_time(date):
-        """
-        Returns dictionary with year, month, day, time, date and time formatted
-        in a friendly way.
-        """
+    """
+    Returns dictionary with year, month, day, time, date and time formatted
+    in a friendly way.
+    """
 
-        year = date.strftime("%Y")
-        month = date.strftime("%m")
-        day = date.strftime("%d")
-        time = date.strftime("%H:%M:%S")
-        formatted_date = date.strftime("%m/%d/%Y")
-        full_date_time = date.strftime("%m/%d/%Y, %H:%M:%S")
+    year = date.strftime("%Y")
+    month = date.strftime("%m")
+    day = date.strftime("%d")
+    time = date.strftime("%H:%M:%S")
+    formatted_date = date.strftime("%m/%d/%Y")
+    full_date_time = date.strftime("%m/%d/%Y, %H:%M:%S")
 
-        # Note: this version of this method now has date in the return, whereas
-        # others don't-- seriously need to break this out next time I work on this
-        return {
-            "year": year,
-            "month": month,
-            "day": day,
-            "time": time,
-            "date": formatted_date,
-            "weekday": calendar.day_name[date.weekday()],
-            "full_date_time": full_date_time
-        }
+    # Note: this version of this method now has date in the return, whereas
+    # others don't-- seriously need to break this out next time I work on this
+    return {
+        "year": year,
+        "month": month,
+        "day": day,
+        "time": time,
+        "date": formatted_date,
+        "weekday": calendar.day_name[date.weekday()],
+        "full_date_time": full_date_time
+    }
 
 # NOTE: leftover test from treatment models tests:
 
